9b1e86dd69e3fd7c923d722ce5031dc7243afa5a
Derek Briggs <derek.briggs@me.com>
2018-12-17 15:22:01 -0500

    Add component defaults and camel casing to `percy-btn` props (#937)
    * percy-btn component defaults
    
    * no angle bracket components…….. yet.
    
    * primary not default

--- app/components/percy-btn.js
+++ app/components/percy-btn.js
@@ -2,6 +2,11 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: '',
+  class: '',
+  isDisabled: false,
+  isPrimaryButton: false,
+  isNoShadow: false,
+  isNoAnimate: false,
 
   actions: {
     handleClick() {
