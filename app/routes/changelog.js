fe6f1dc9e8078b5dcc0957a5c9e835e1338c1c71
Lindsay <lrc3233@gmail.com>
2019-02-05 10:50:13 -0800

    use contentful to load hero (#992)
    * changelog header update
    
    * use contentful to load hero

--- app/routes/changelog.js
+++ app/routes/changelog.js
@@ -1,11 +1,24 @@
 import Route from '@ember/routing/route';
 import metaTagLookup from 'percy-web/lib/meta-tags';
+import {hash} from 'rsvp';
 
 export default Route.extend({
   headTags: metaTagLookup('changelog'),
   model() {
-    return this.store.findAll('changelog-post').then(posts => {
+    const hero = this.store.queryRecord('heroBlock', {
+      'fields.page': 'Changelog',
+    });
+    const posts = this.store.findAll('changelog-post').then(posts => {
       return posts.sortBy('date').reverse();
+    });
+
+    return hash({hero, posts});
+  },
+
+  setupController(controller, model) {
+    controller.setProperties({
+      hero: model.hero,
+      posts: model.posts,
     });
   },
 
