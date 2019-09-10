682a73a1b1bf20023a8661396eda5aac8cbd91a6
Rylee Keys <rylee@percy.io>
2019-09-09 18:57:52 -0600

    Update websockets/auth route to not be under v1 (#1369)

--- config/environment.js
+++ config/environment.js
@@ -60,7 +60,7 @@ module.exports = function(environment) {
         headAsset: '/api/v1/snapshots/%@/assets/head.html',
         buildSnapshots: '/api/v1/builds/%@/snapshots',
         snapshotSourceDiff: '/api/v1/comparisons/%@/diffs/source.diff',
-        websocketsAuth: '/api/v1/websockets/auth',
+        websocketsAuth: '/api/websockets/auth',
       },
       githubUrls: {
         integration: 'https://github.com/apps/percy/installations/new',
