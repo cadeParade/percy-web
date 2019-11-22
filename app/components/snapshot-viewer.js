6329ef208df54d2fc1eb838d7c042399a4ce91ac
Derek Briggs <derek.briggs@me.com>
2019-11-22 13:20:51 -0500

    Overlay click tracking (#1467)
    * click hint badge only shows initially
    
    * switch to localstorageproxy

--- app/components/snapshot-viewer.js
+++ app/components/snapshot-viewer.js
@@ -2,6 +2,7 @@ import {filterBy, notEmpty, or, readOnly} from '@ember/object/computed';
 import {computed} from '@ember/object';
 import SnapshotListItem from 'percy-web/components/snapshot-list-item';
 import {inject as service} from '@ember/service';
+import localStorageProxy from 'percy-web/lib/localstorage';
 
 export default SnapshotListItem.extend({
   launchDarkly: service(),
@@ -42,10 +43,31 @@ export default SnapshotListItem.extend({
   openCommentThreads: filterBy('commentThreads', 'isOpen'),
 
   isSnapshotShowingDiffOverlay: true,
+
+  setHiddenClickBadge() {
+    this.set('isSnapshotDiffOverlayShowingClickBadge', false);
+  },
+
+  isSnapshotDiffOverlayShowingClickBadge: true,
+  diffOverlayToggleClickAmount: 0,
+
+  didInsertElement() {
+    this._super(...arguments);
+    if (localStorageProxy.get('diffOverlayToggleClickAmount') > 2) {
+      this.setHiddenClickBadge();
+    }
+  },
+
   actions: {
     toggleSnapshotOverlay() {
       this.toggleProperty('isSnapshotShowingDiffOverlay');
       this.trackToggleOverlay(this.isSnapshotShowingDiffOverlay);
+
+      if (localStorageProxy.get('diffOverlayToggleClickAmount') > 2) {
+        this.setHiddenClickBadge();
+      }
+      this.diffOverlayToggleClickAmount++;
+      localStorageProxy.set('diffOverlayToggleClickAmount', this.diffOverlayToggleClickAmount);
     },
 
     toggleCollaborationPanel() {
