f689d4568b75f7e45b68821513f3a8fcc3176855
Robert DeLuca <robertdeluca19@gmail.com>
2019-12-20 15:01:14 -0600

    Update helper

--- tests/helpers/percy-snapshot.js
+++ tests/helpers/percy-snapshot.js
@@ -3,19 +3,17 @@ import percySnapshotOg from '@percy/ember';
 const DARK_MODE_CLASS_NAME = 'theme-dark';
 
 export default async function percySnapshot(name, options = {}) {
+  await percySnapshotOg(name, options);
+
   if (options.darkMode) {
     document.body.classList.add(DARK_MODE_CLASS_NAME);
-    await percySnapshotOg(processName(name), options);
+    await percySnapshotOg(darkName(name), options);
     document.body.classList.remove(DARK_MODE_CLASS_NAME);
-
-    return;
   }
-
-  return await percySnapshotOg(name, options);
 }
 
 // Prepends "Dark" in front of the normal auto name gen we do
-function processName(name) {
+function darkName(name) {
   // Automatic name generation for QUnit tests by passing in the `assert` object.
   if (name.test && name.test.module && name.test.module.name && name.test.testName) {
     return `Dark${name.test.module.name} | ${name.test.testName}`;
@@ -23,6 +21,6 @@ function processName(name) {
     // Automatic name generation for Mocha tests by passing in the `this.test` object.
     return `Dark${name.fullTitle()}`;
   } else {
-    return name;
+    return `Dark${name}`;
   }
 }
