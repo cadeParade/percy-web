3d4634937792474631df1a5a78d90a251b531287
Robert DeLuca <robertdeluca19@gmail.com>
2019-12-20 12:02:01 -0600

    fix: Auto snapshot name generation

--- tests/helpers/percy-snapshot.js
+++ tests/helpers/percy-snapshot.js
@@ -5,7 +5,7 @@ const DARK_MODE_CLASS_NAME = 'theme-dark';
 export default async function percySnapshot(name, options = {}) {
   if (options.darkMode) {
     document.body.classList.add(DARK_MODE_CLASS_NAME);
-    await percySnapshotOg(processName(`Dark ${name}`), options);
+    await percySnapshotOg(processName(name), options);
     document.body.classList.remove(DARK_MODE_CLASS_NAME);
 
     return;
@@ -14,14 +14,15 @@ export default async function percySnapshot(name, options = {}) {
   return await percySnapshotOg(name, options);
 }
 
+// Prepends "Dark" in front of the normal auto name gen we do
 function processName(name) {
   // Automatic name generation for QUnit tests by passing in the `assert` object.
   if (name.test && name.test.module && name.test.module.name && name.test.testName) {
-    return `${name.test.module.name} | ${name.test.testName}`;
+    return `Dark ${name.test.module.name} | ${name.test.testName}`;
   } else if (name.fullTitle) {
     // Automatic name generation for Mocha tests by passing in the `this.test` object.
-    return name.fullTitle();
+    return `Dark ${name.fullTitle()}`;
   } else {
-    return name;
+    return `Dark ${name}`;
   }
 }
