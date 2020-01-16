e98cdab2d7b0674c92e1ed30f952c88c171087dd
LC <lrc3233@gmail.com>
2020-01-15 17:01:57 -0800

    no-unused-block-params, no-quoteless-attributes

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -12,7 +12,5 @@ module.exports = {
     'table-groups': false,
     'require-button-type': false,
     'no-negated-condition': false,
-    'no-quoteless-attributes': false,
-    'no-unused-block-params': false,
   }
 };
