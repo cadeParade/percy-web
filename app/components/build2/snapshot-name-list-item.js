584450d6ebfe35b9c4f8b1e64bce453a070b8d6c
LC <lrc3233@gmail.com>
2020-02-12 17:28:05 -0800

    scroll sidebar to active thumbnail

---
+++ app/components/build2/snapshot-name-list-item.js
@@ -0,0 +1,22 @@
+import Component from '@ember/component';
+import {inject as service} from '@ember/service';
+import {observer} from '@ember/object';
+
+export default Component.extend({
+  router: service(),
+
+  // eslint-disable-next-line
+  scrollToTop: observer('router.currentURL', 'snapshot.id', function() {
+    if (this.router.currentURL.includes(this.snapshot.id)) {
+      var myElement = document.getElementById(this.elementId);
+      var topPos = myElement.offsetTop;
+      document.getElementById('snapshot-list-sidebar').scrollTop = topPos - 190;
+    }
+  }),
+
+  init() {
+    this._super(...arguments);
+    // eslint-disable-next-line
+    this.router.currentURL;
+  },
+});
