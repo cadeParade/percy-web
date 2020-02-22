2fc7903c43e58ad162a24061611ee012cf6db30a
LC <lrc3233@gmail.com>
2020-02-21 18:10:55 -0800

    add props to display unchanged snapshot box

--- app/components/infinite-snapshot-list.js
+++ app/components/infinite-snapshot-list.js
@@ -1,7 +1,14 @@
 import Component from '@ember/component';
+import {or, alias, readOnly} from '@ember/object/computed';
+import {computed} from '@ember/object';
 
 export default Component.extend({
   page: 0,
+
+  numSnapshotsChanged: readOnly('orderItems.items.length'),
+  numSnapshotsUnchanged: computed('build.totalSnapshots', 'numSnapshotsChanged', function() {
+    return this.build.totalSnapshots - this.numSnapshotsChanged;
+  }),
 
   actions: {
     noop() {},
