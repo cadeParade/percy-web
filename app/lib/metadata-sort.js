193f2948c2bde22ababa33767c075674eca5af67
LC <lrc3233@gmail.com>
2020-02-21 17:39:12 -0800

    new build, add unchanged block

--- app/lib/metadata-sort.js
+++ app/lib/metadata-sort.js
@@ -1,66 +1,140 @@
 // request snapshots
 // get back 1st 10 and a bunch of metadata
 // build id 4138253
+
+// percy-web build number 8577
+// https://percy.io/percy/percy-web/builds/4024410
+
+export default {
+  items: [
+    {
+      index: 0,
+      type: 'snapshot',
+      id: 238806340,
+    },
+    {
+      index: 1,
+      type: 'group',
+      fingerprint: 'slkjrwelkrjwe',
+      cover: {
+        type: 'snapshot',
+        id: 238806297,
+      },
+      'snapshot-ids': [238806297, 238806312],
+    },
+    {
+      index: 2,
+      type: 'snapshot',
+      id: 238804587,
+    },
+    {
+      index: 3,
+      type: 'snapshot',
+      id: 238804346,
+    },
+    {
+      index: 3,
+      type: 'snapshot',
+      id: 238804614,
+    },
+    {
+      index: 4,
+      type: 'snapshot',
+      id: 238804627,
+    },
+    {
+      index: 5,
+      type: 'snapshot',
+      id: 238804403,
+    },
+    {
+      index: 6,
+      type: 'snapshot',
+      id: 238804415,
+    },
+    {
+      index: 7,
+      type: 'snapshot',
+      id: 238804485,
+    },
+    {
+      index: 8,
+      type: 'snapshot',
+      id: 238804412,
+    },
+    {
+      index: 9,
+      type: 'snapshot',
+      id: 238804400,
+    },
+    {
+      index: 8,
+      type: 'snapshot',
+      id: 238804622,
+    },
+  ],
+};
+
 // percy-web build number 8622
 // http://dev.percy.local:4200/percy/percy-web/builds/4138253
-export default {
- 'items': [
-   {
-     'index': 0,
-     'type': 'group',
-     'cover': {
-       'type': 'snapshot',
-       'id': 244809930,
-     },
-     'snapshot-ids': [244809938, 244809939, 244809933, 244809930],
-   },
-   {
-     'index': 1,
-     'type': 'group',
-     'cover': {
-       'type': 'snapshot',
-       'id': 244809748,
-     },
-     'snapshot-ids': [244809737, 244809739, 244809748],
-   },
-   {
-     'index': 2,
-     'type': 'group',
-     'cover': {
-       'type': 'snapshot',
-       'id': 244809927,
-     },
-     'snapshot-ids': [244809743, 244809745, 244809927],
-   },
-   {
-     'index': 3,
-     'type': 'snapshot',
-     'id': 244809937,
-   },
-   {
-     'index': 4,
-     'type': 'snapshot',
-     'id': 244810133,
-   },
-   {
-     'index': 5,
-     'type': 'snapshot',
-     'id': 244811892,
-   },
-   {
-     'index': 6,
-     'type': 'snapshot',
-     'id': 244811896,
-   },
-   {
-     'index': 7,
-     'type': 'snapshot',
-     'id': 244814362,
-   },
-   {
-     'index': 8,
-     'type': 'snapshot',
-     'id': 244814363,
-   },
- ],
-}
+// export default {
+//  'items': [
+//    {
+//      'index': 0,
+//      'type': 'group',
+//      'cover': {
+//        'type': 'snapshot',
+//        'id': 244809930,
+//      },
+//      'snapshot-ids': [244809938, 244809939, 244809933, 244809930],
+//    },
+//    {
+//      'index': 1,
+//      'type': 'group',
+//      'cover': {
+//        'type': 'snapshot',
+//        'id': 244809748,
+//      },
+//      'snapshot-ids': [244809737, 244809739, 244809748],
+//    },
+//    {
+//      'index': 2,
+//      'type': 'group',
+//      'cover': {
+//        'type': 'snapshot',
+//        'id': 244809927,
+//      },
+//      'snapshot-ids': [244809743, 244809745, 244809927],
+//    },
+//    {
+//      'index': 3,
+//      'type': 'snapshot',
+//      'id': 244809937,
+//    },
+//    {
+//      'index': 4,
+//      'type': 'snapshot',
+//      'id': 244810133,
+//    },
+//    {
+//      'index': 5,
+//      'type': 'snapshot',
+//      'id': 244811892,
+//    },
+//    {
+//      'index': 6,
+//      'type': 'snapshot',
+//      'id': 244811896,
+//    },
+//    {
+//      'index': 7,
+//      'type': 'snapshot',
+//      'id': 244814362,
+//    },
+//    {
+//      'index': 8,
+//      'type': 'snapshot',
+//      'id': 244814363,
+//    },
+//  ],
+// }
